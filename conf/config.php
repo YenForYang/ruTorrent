1653c1259a3aa2bacd2cdd89f4d6a33e2cbc2b0a
YenForYang <YenForYang@users.noreply.github.com>
2018-08-31 13:03:53 -0500

    Update config.php
    For personal usage

--- conf/config.php
+++ conf/config.php
@@ -27,25 +27,26 @@
 	$topDirectory = '/';			// Upper available directory. Absolute path with trail slash.
 	$forbidUserSettings = false;
 
-	$scgi_port = 5000;
-	$scgi_host = "127.0.0.1";
+	// $scgi_port = 5000;
+	// $scgi_host = "127.0.0.1";
 
 	// For web->rtorrent link through unix domain socket 
 	// (scgi_local in rtorrent conf file), change variables 
 	// above to something like this:
 	//
 	// $scgi_port = 0;
-	// $scgi_host = "unix:///tmp/rpc.socket";
 
+	$scgi_host = "unix:///home/richard/rtorrent/.scgi_local"
+		
 	$XMLRPCMountPoint = "/RPC2";		// DO NOT DELETE THIS LINE!!! DO NOT COMMENT THIS LINE!!!
 
 	$pathToExternals = array(
-		"php" 	=> '',			// Something like /usr/bin/php. If empty, will be found in PATH.
-		"curl"	=> '',			// Something like /usr/bin/curl. If empty, will be found in PATH.
-		"gzip"	=> '',			// Something like /usr/bin/gzip. If empty, will be found in PATH.
-		"id"	=> '',			// Something like /usr/bin/id. If empty, will be found in PATH.
-		"stat"	=> '',			// Something like /usr/bin/stat. If empty, will be found in PATH.
-	);
+                "php"   => '/usr/bin/php',                      // Something like /usr/bin/php. If empty, will be found in PATH.
+                "curl"  => '/usr/bin/curl',                     // Something like /usr/bin/curl. If empty, will be found in PATH.
+                "gzip"  => '/usr/bin/gzip',                     // Something like /usr/bin/gzip. If empty, will be found in PATH.
+                "id"    => '/usr/bin/id',                       // Something like /usr/bin/id. If empty, will be found in PATH.
+                "stat"  => '/usr/bin/stat',                     // Something like /usr/bin/stat. If empty, will be found in PATH.
+        );
 
 	$localhosts = array( 			// list of local interfaces
 		"127.0.0.1",
@@ -59,6 +60,6 @@
 
 	$tempDirectory = null;			// Temp directory. Absolute path with trail slash. If null, then autodetect will be used.
 
-	$canUseXSendFile = false;		// If true then use X-Sendfile feature if it exist
+	$canUseXSendFile = true;		// If true then use X-Sendfile feature if it exist
 
 	$locale = "UTF8";
